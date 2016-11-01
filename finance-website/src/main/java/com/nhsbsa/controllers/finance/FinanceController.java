5ed45f4ac2bc19685618f113ecd72688d98fbda3
jeffreya <andrew-jeffrey@live.co.uk>
2016-11-01 14:40:39 +0000

    created finance html created finance controller redirected to finance when authenticated fixed url to point to correct service

--- finance-website/src/main/java/com/nhsbsa/controllers/finance/FinanceController.java
+++ finance-website/src/main/java/com/nhsbsa/controllers/finance/FinanceController.java
@@ -1,9 +1,30 @@
 package com.nhsbsa.controllers.finance;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jeffreya on 26/10/2016.
  *
  */
 
+@Controller
+@RequestMapping("/finance")
 public class FinanceController {
+
+    public FinanceController() {
+
+    }
+
+    @GetMapping
+    public ModelAndView getForm() {
+        final Map<String, Object> model = new HashMap<>();
+        return new ModelAndView("finance", model);
+    }
 }
