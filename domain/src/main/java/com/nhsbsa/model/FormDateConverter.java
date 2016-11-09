f2525871a0e09229871418d4867c97b7990de1ab
nhulse <nat.hulse@nhs.net>
2016-11-09 11:44:13 +0000

    Following Validations added for Transfer Date: 1. Date in correct format. 2. Date must b greater than today. 3. Date cannot be greater than 31 days in the future.

--- domain/src/main/java/com/nhsbsa/model/FormDateConverter.java
+++ domain/src/main/java/com/nhsbsa/model/FormDateConverter.java
@@ -13,7 +13,7 @@ public class FormDateConverter implements AttributeConverter<FormDate, Date> {
 
     @Override
     public Date convertToDatabaseColumn(FormDate formDate) {
-        return formDate.toDate();
+        return formDate.getDate();
     }
 
     @Override
