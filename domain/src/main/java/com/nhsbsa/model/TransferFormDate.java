6a55963b1f825438d1ea967a92c0f670b9f05e02
Mark <mark.lishman@gmail.com>
2016-11-10 07:56:19 +0000

    Re-factored schedule payment validation

--- domain/src/main/java/com/nhsbsa/model/TransferFormDate.java
+++ domain/src/main/java/com/nhsbsa/model/TransferFormDate.java
@@ -2,6 +2,7 @@ package com.nhsbsa.model;
 
 import com.nhsbsa.model.validaton.DateIsAfterToday;
 import com.nhsbsa.model.validaton.DateLessThan31DaysFromToday;
+import com.nhsbsa.model.validaton.FormDateNotBlank;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -11,10 +12,10 @@ import java.util.Date;
  * Created by Mark Lishman on 07/11/2016.
  */
 
+@FormDateNotBlank
 public class TransferFormDate extends FormDate {
 
     @Override
-    @NotNull (message = "{transferDate.notBlank}")
     @DateIsAfterToday
     @DateLessThan31DaysFromToday
     public Date getDate() {
