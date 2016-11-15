5e563656224d21fa537a0987e94b9c17eed331b2
MattHood <matthewroyhood@gmail.com>
2016-11-15 13:57:41 +0000

    Added liquibase create table script and added JPA annotations

---
+++ dao/src/main/java/com/nhsbsa/service/CSVService.java
@@ -0,0 +1,16 @@
+package com.nhsbsa.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.xml.ws.ServiceMode;
+
+/**
+ * Created by MattHood on 15/11/2016.
+ */
+
+@Service
+public class CSVService {
+
+
+
+}
