45c15849dffbf321f486d3f04d689cc68ca49705
Mark <mark.lishman@gmail.com>
2016-12-09 08:41:23 +0000

    Fixed problem with Employee Added Years

--- automation-tests/src/test/java/com/nhsbsa/CucumberRunnerTest.java
+++ automation-tests/src/test/java/com/nhsbsa/CucumberRunnerTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 @Cucumber.Options(
         format = {"pretty", "html:reports/test-report"},
 
-        tags = {"@adjustments"})
+        tags = {"@smokeTest"})
 
 public class CucumberRunnerTest {
 
